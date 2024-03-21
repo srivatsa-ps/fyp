968bbd122e133f72287690dd3095d985592f706b
Swaminathan <91941849+Random-Userr@users.noreply.github.com>
2024-03-21 19:35:05 +0530

    Create ShapegameData.dart

---
+++ lib/Games/ShapegameData.dart
@@ -0,0 +1,11 @@
+class Itemdata {
+  final String name;
+  final String address;
+  Itemdata({required this.name, required this.address});
+}
+
+List<Itemdata> itemlist = [
+  Itemdata(name: "1", address: "assets/images/octo.svg"),
+  Itemdata(name: "2", address: "assets/images/shark.svg"),
+  Itemdata(name: "3", address: "assets/images/whale.svg")
+];
