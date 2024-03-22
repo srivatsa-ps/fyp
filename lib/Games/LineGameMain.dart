66236ab2458209bf526dea54c4c0c203dc3d8cd6
srivatsa-ps <ps.srivatsa2015@gmail.com>
2024-03-22 20:23:34 +0530

    21/3 night added game drawer and pause functionality to line game

--- lib/Games/LineGameMain.dart
+++ lib/Games/LineGameMain.dart
@@ -1,29 +1,110 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:fyp/components/game_drawer.dart';
+import '../pages/landing_page.dart';
+import 'gamesmain.dart';
 
-void main() {
-  runApp(NumberTracingApp());
+class NumberTracingApp extends StatefulWidget {
+  @override
+  State<NumberTracingApp> createState() => _NumberTracingAppState();
 }
 
-class NumberTracingApp extends StatelessWidget {
+class _NumberTracingAppState extends State<NumberTracingApp> {
+  late Random random;
+  late int randomNumber;
+  late List<DrawingArea> points;
+
+  @override
+  void initState() {
+    super.initState();
+    random = Random();
+    randomNumber = 0;
+    points = [];
+    _resetGame(); // Initialize the game state
+  }
+
+  void _resetGame() {
+    setState(() {
+      randomNumber = random.nextInt(10);
+      points.clear();
+    });
+  }
+
+  void goToHomePage() {
+    Navigator.pop(context);
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => HomePage()),
+    );
+  }
+
+
+  void goToGamePage() {
+    Navigator.pop(context);
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => GamesListPage()),
+    );
+  }
+
+  void showPauseDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("MENU"),
+          content: Text("Game is Paused. What would you like to do?"),
+          actions: <Widget>[
+            TextButton(
+              child: Text(
+                "Restart",
+                style: TextStyle(color: Colors.grey[900]),
+              ),
+              onPressed: () {
+                Navigator.of(context).pop();
+                _resetGame();
+              },
+            ),
+            TextButton(
+              child: Text(
+                "Cancel",
+                style: TextStyle(color: Colors.grey[900]),
+              ),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: Text(
+                "Quit",
+                style: TextStyle(color: Colors.grey[900]),
+              ),
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the dialog
+                Navigator.pop(context); // Navigate back to the previous screen
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.lightBlue.shade100, // Light color background
+        backgroundColor: Colors.lightBlue.shade100,
         appBar: AppBar(
-          backgroundColor:
-              Colors.lightBlue.shade300, // Match AppBar theme with app
-          centerTitle: true, // Center the title or in this case, the IconButton
+          backgroundColor: Colors.lightBlue.shade300,
+          centerTitle: true,
           title: IconButton(
             icon: Icon(Icons.pause_circle_filled, color: Colors.white),
-            onPressed: () {
-              // Accessing the context to show the dialog, this needs to call showPauseDialog from NumberTracingPage
-              // This action is simulated here as direct access to showPauseDialog isn't straightforward due to context
-              // You would typically use a state management solution or callbacks to handle this
-            },
+            onPressed: showPauseDialog,
           ),
         ),
+          drawer: MyGameDrawer(onHomeTap: goToHomePage,
+            onGameTap: goToGamePage,),
         body: SafeArea(
           child: NumberTracingPage(),
         ),
@@ -42,51 +123,17 @@ class _NumberTracingPageState extends State<NumberTracingPage> {
   int randomNumber = 0;
   List<DrawingArea> points = [];
 
-  void resetGame() {
+  void _resetGame() {
     setState(() {
       randomNumber = random.nextInt(10);
       points.clear();
     });
   }
 
-  void showPauseDialog() {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text("Game Options"),
-          content: Text("What would you like to do?"),
-          actions: <Widget>[
-            TextButton(
-              child: Text("Restart"),
-              onPressed: () {
-                resetGame();
-                Navigator.of(context).pop();
-              },
-            ),
-            TextButton(
-              child: Text("Resume"),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-            TextButton(
-              child: Text("Quit"),
-              onPressed: () {
-                // Assuming Navigator.pushReplacement or similar to navigate away
-                // This is a placeholder action
-              },
-            ),
-          ],
-        );
-      },
-    );
-  }
-
   @override
   void initState() {
     super.initState();
-    resetGame();
+    _resetGame();
   }
 
   @override
@@ -98,7 +145,7 @@ class _NumberTracingPageState extends State<NumberTracingPage> {
           flex: 1,
           child: Center(
             child: GestureDetector(
-              onTap: resetGame,
+              onTap: _resetGame,
               child: Text(
                 '$randomNumber',
                 style: TextStyle(fontSize: 150, fontWeight: FontWeight.bold),
@@ -135,7 +182,7 @@ class LineDraw extends StatefulWidget {
 class _LineDrawState extends State<LineDraw> {
   Color selectedColor = Colors.black;
 
-  void updatePoints(DrawingArea newPoint) {
+  void _updatePoints(DrawingArea newPoint) {
     widget.onPointsUpdate(List.from(widget.points)..add(newPoint));
   }
 
@@ -143,25 +190,30 @@ class _LineDrawState extends State<LineDraw> {
   Widget build(BuildContext context) {
     return GestureDetector(
       onPanDown: (details) {
-        updatePoints(DrawingArea(
-            point: details.localPosition,
-            areaPaint: Paint()
-              ..strokeCap = StrokeCap.round
-              ..isAntiAlias = true
-              ..color = selectedColor
-              ..strokeWidth = 2.0));
+        _updatePoints(DrawingArea(
+          point: details.localPosition,
+          areaPaint: Paint()
+            ..strokeCap = StrokeCap.round
+            ..isAntiAlias = true
+            ..color = selectedColor
+            ..strokeWidth = 2.0,
+        ));
       },
       onPanUpdate: (details) {
-        updatePoints(DrawingArea(
-            point: details.localPosition,
-            areaPaint: Paint()
-              ..strokeCap = StrokeCap.round
-              ..isAntiAlias = true
-              ..color = selectedColor
-              ..strokeWidth = 2.0));
+        _updatePoints(DrawingArea(
+          point: details.localPosition,
+          areaPaint: Paint()
+            ..strokeCap = StrokeCap.round
+            ..isAntiAlias = true
+            ..color = selectedColor
+            ..strokeWidth = 2.0,
+        ));
       },
       onPanEnd: (details) {
-        updatePoints(DrawingArea(point: Offset.zero, areaPaint: Paint()));
+        _updatePoints(DrawingArea(
+          point: Offset.zero,
+          areaPaint: Paint(),
+        ));
       },
       child: CustomPaint(
         size: Size.infinite,
@@ -173,6 +225,7 @@ class _LineDrawState extends State<LineDraw> {
 
 class MyCustomPainter extends CustomPainter {
   List<DrawingArea> points;
+
   MyCustomPainter({required this.points});
 
   @override
@@ -183,7 +236,10 @@ class MyCustomPainter extends CustomPainter {
           points[i].point != Offset.zero &&
           points[i + 1].point != Offset.zero) {
         canvas.drawLine(
-            points[i].point, points[i + 1].point, points[i].areaPaint);
+          points[i].point,
+          points[i + 1].point,
+          points[i].areaPaint,
+        );
       }
     }
   }
@@ -195,5 +251,6 @@ class MyCustomPainter extends CustomPainter {
 class DrawingArea {
   Offset point;
   Paint areaPaint;
+
   DrawingArea({required this.point, required this.areaPaint});
 }
